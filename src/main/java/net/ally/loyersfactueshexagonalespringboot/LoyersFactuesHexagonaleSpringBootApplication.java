af1c960d4f6b686f66c7d221f050adcade421ce3
allyRx <herindrainyall2003@gmail.com>
2026-01-27 16:34:36 +0300

    init project

---
+++ src/main/java/net/ally/loyersfactueshexagonalespringboot/LoyersFactuesHexagonaleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package net.ally.loyersfactueshexagonalespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyersFactuesHexagonaleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyersFactuesHexagonaleSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ally/loyersfactueshexagonalespringboot/LoyersFactuesHexagonaleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ally.loyersfactueshexagonalespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyersFactuesHexagonaleSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
